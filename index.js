89acbcc73198ee66f76a979961690f6df97e87da
Steven Liu <steeven.liu2@gmail.com>
2019-10-17 14:56:50 +1100

    getting there

--- index.js
+++ index.js
@@ -11,27 +11,130 @@ take most recent one, always
 */
 
 const dgram = require('dgram');
-const server = dgram.createSocket('udp4');
 
-server.bind(11123,undefined,()=>{
-    server.setBroadcast(true);
+function _nanogram(id, _options) {
+    const server = dgram.createSocket('udp4');
+    let options = {
+        udpPort: 11233,
+        transmitPort: -1,
+    }
+    Object.assign(options, _options);
+    if (options.transmitPort < -1) options.transmitPort = options.udpPort + Date.now() % (65534 - udpPort);
+    //add an event api
+    this.events = {};
+    this.fire = (e, args) => {
+        let _e = e.split(",");
+        _e.push("*"); // a wildcard event listener
+        _e.forEach((i) => {
+            if (!this.events[i]) return;
+            if (this.events[i].events) {
+                this.events[i].events.forEach((f) => {
+                    try {
+                        f(args)
+                    } catch (er) {
+                        console.log(er);
+                    }
+
+                });
+            }
+        })
+    };
+    this.on = (e, f) => {
+        let _e = e.split(',');
+        _e.forEach((i) => {
+            if (!this.events[i]) this.events[i] = {};
+            if (!this.events[i].events) this.events[i].events = [];
+            this.events[i].events.push(f);
+        })
+    };
+
+    let message;
+    function encodeList(data) {
+        //hash the list.
+        let asString = JSON.stringify(data);
+        let charbox = [];
+        let place = 0;
+        for (let i = 0; i < asString.length; i++) {
+            if (charbox.length < place + 1) charbox.push(0);
+            charbox[place] += asString.charCodeAt(i);
+            charbox[place] %= 49;
+            place++;
+            place %= 64;
+        }
+        charbox = charbox.map((i) => String.fromCharCode(i + 48));
+        return charbox.join("");
+    }
+
+
+    let nanogramReady = false;
+    server.bind(options.udpPort, undefined, () => {
+        server.setBroadcast(true);
+        nanogramReady = true;
+    })
+
+    server.on('error', (err) => {
+        console.log(`server error:\n${err.stack}`);
+        server.close();
+    });
+
+    server.on('message', (msg, rinfo) => {
+        console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
+        if (message!=msg && message.split("|")[1]==msg.split("|")[1]){
+            //fire me at their tcp
+        }
+    });
+
+    server.on('listening', () => {
+        const address = server.address();
+        console.log(`server listening ${address.address}:${address.port}`);
+        let message = "hello world!";
+    });
+
+    let previousBroadcastTimer = undefined;
+    this.on('newData',()=>{
+        if (nanogramReady) {
+            //start broadcasting
+            if (previousBroadcastTimer != undefined) {
+                clearInterval(previousBroadcastTimer);
+            }
+            message = encodeList(data);// data e.g. ASFWEVIASERNASFDLASGJAWEO
+            message += "|";
+            message += id//id of the document. IDK.
+            previousBroadcastTimer = setInterval(() => {
+                server.send(message, 0, message.length, options.udpPort, "255.255.255.255");
+            }, 1000);
+        }
+    })
+
+    //now for TCP connections
+    //create new tcp listener
+    //when tcp listener receives data, merge it.
+
+
+
+}
+
+
+
+
+///////////////usage
+/*
+nanogram.on('update',(newdata)=>{
+    //the new data, for you to do as you will.
 })
 
-server.on('error', (err) => {
-    console.log(`server error:\n${err.stack}`);
-    server.close();
-});
+nanogram.fire('newData',data);
+//push data to nanogram.
 
-server.on('message', (msg, rinfo) => {
-    console.log(`server got: ${msg} from ${rinfo.address}:${rinfo.port}`);
-});
+let data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
+//data MUST be a javascript object. if you just need an array, make it like {0: item0, 1: item1} etc.
+// for consistency's sake.
 
-server.on('listening', () => {
-    const address = server.address();
-    console.log(`server listening ${address.address}:${address.port}`);
-    let message="hello world!";
-    server.send(message,0,message.length,11123,"255.255.255.255");
-});
+*/
+
+
+
+
 
 
 // Prints: server listening 0.0.0.0:41234
@@ -39,7 +142,7 @@ server.on('listening', () => {
 
 
 //occasionally, broadcast on UDP to others
-//broadcast IP, document name, revision hash, port i can listen on
+//document name, revision hash, port i can listen on
 
 // listen for udp broadcasts
 // if hear one, compare to current hash. 
